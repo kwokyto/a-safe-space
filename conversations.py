2bdf4ec3517e950c499b93630ca41ce5010fe680
kwokyto <ryankwok@u.nus.edu>
2021-06-22 21:58:25 +0800

    Test commit

--- conversations.py
+++ conversations.py
@@ -5,10 +5,13 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
 
 def start(update, context):
     """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi!')
+    reply = str(context.chat_data)
+    update.message.reply_text(reply)
 
 def error(update, context):
     # Log Errors caused by Updates.
