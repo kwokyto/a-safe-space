db6ea7632721f75953be23a99309e41f41bc7380
kwokyto <ryankwok@u.nus.edu>
2021-06-27 23:05:22 +0800

    Update constants.py
    #1

--- asafespace/constants.py
+++ asafespace/constants.py
@@ -1,25 +1,83 @@
+# Admin sprecfic links
+REGISTRATION_LINK = "REGISTRATION_LINK"
+FAQ_LINK = "FAQ_LINK"
+ADMIN_HANDLE = "@kwokyto"
+
+
+
+# Messages sent to the user
 PREFIX = "A Safe Space:\n"
 
-INVALID_FORMAT_MESSAGE = PREFIX + "Sorry, only texts messages or stickers are allowed. Editing of messages are also not allowed."
-START_MESSAGE = PREFIX + "Welcome to A Safe Space by Love, USP! Enter /register to register."
-HELP_MESSAGE = PREFIX + "help message"
-ALREADY_REGISTERED_MESSAGE = PREFIX + "already registered messsage"
-REGISTRATION_CLARIFICATION_MESSAGE = PREFIX + "registration clarification message"
-WRONG_PASSWORD_MESSAGE = PREFIX + "wrong password message"
-REGISTRATION_SUCCESS_MESSAGE = PREFIX + "registration success message"
-REGISTRATION_FAILURE_MESSAGE = PREFIX + "registration failure message"
-UNAUTHORISED_MESSAGE = PREFIX + "unauthorised message"
-USERNAME_MESSAGE = PREFIX + "username message"
-LEAVE_SUCCESS_MESSAGE = PREFIX + "leave success message"
-LEAVE_FAILURE_MESSAGE = PREFIX + "leave failure message"
-INVALID_COMMAND_MESSAGE = PREFIX + "invalid command message"
-SENT_STICKER_MESSAGE = PREFIX + "sent a sticker message"
-NOT_ADMIN_MESSAGE = PREFIX + "not an admin message"
-ADMIN_REMOVE_SUCCESS_MESSAGE = PREFIX + "admin remove success message"
-ADMIN_REMOVE_FAILURE_MESSAGE = PREFIX + "admin remove failure message"
-INVALID_ADMIN_COMMAND_MESSAGE = PREFIX + "invalid admin command message"
-UNDER_MAINTENANCE_MESSAGE = PREFIX + "under maintenance message"
+INVALID_FORMAT_MESSAGE = PREFIX + \
+    "Sorry, only texts messages or stickers are allowed. Editing of messages are also not allowed."
 
+START_MESSAGE = PREFIX + \
+    "Welcome! If you have not done so, do fill up the registration form at " + REGISTRATION_LINK + "\n"\
+    "Then, you can use /register to register!"
+
+HELP_MESSAGE = PREFIX + \
+    "I was created as a platform for USP students to reach out for help and seek conversations on mental health issues. " + \
+    "I simulate a group chat where students can share experience or advice, while remaining completely anonymous.\n\n" + \
+    "Sign Up Form: " + REGISTRATION_LINK + "\n" + \
+    "FAQ: " + FAQ_LINK + "\n" + \
+    "Feel free to contact " + ADMIN_HANDLE + " if you have any queries!"
+
+ALREADY_REGISTERED_MESSAGE = PREFIX + "You are already registered!"
+
+REGISTRATION_CLARIFICATION_MESSAGE = PREFIX + \
+    "To register, add your NUSNET ID and password behind the register command. For example:\n\n" + \
+    "    /register e1234567 sUp3rsEcr3tPa55w0RD\n\n" + \
+    "To get your password, fill up the form at " + REGISTRATION_LINK + " and wait for our admins to contact you."
+
+WRONG_PASSWORD_MESSAGE = PREFIX + \
+    "Incorrect NUSNET ID or password." + \
+    "To register, add your NUSNET ID and password behind the register command. For example:\n\n" + \
+    "    /register e1234567 sUp3rsEcr3tPa55w0RD\n\n" + \
+    "If you believe that there is a problem, do reach out to " + ADMIN_HANDLE + "."
+
+REGISTRATION_SUCCESS_MESSAGE = PREFIX + \
+    "Yay! You are now registered and can send and receive messages in this chat. " + \
+    "Your username is "
+
+REGISTRATION_FAILURE_MESSAGE = PREFIX + \
+    "Something went wrong with your registration. Please contact " + ADMIN_HANDLE + " as soon as you can."
+
+UNAUTHORISED_MESSAGE = PREFIX + \
+    "You are not yet registered! " + \
+    "To register, add your NUSNET ID and password behind the register command. For example:\n\n" + \
+    "    /register e1234567 sUp3rsEcr3tPa55w0RD\n\n" + \
+    "To get your password, fill up the form at " + REGISTRATION_LINK + " and wait for our admins to contact you."
+
+USERNAME_MESSAGE = PREFIX + \
+    "Your username is "
+
+LEAVE_SUCCESS_MESSAGE = PREFIX + \
+    "We are sad to see you go! Use the same password if you wish to re-register."
+
+LEAVE_FAILURE_MESSAGE = PREFIX + \
+    "Something went wrong when you tried to leave the bot. Please contact " + ADMIN_HANDLE + " as soon as you can."
+
+INVALID_COMMAND_MESSAGE = PREFIX + \
+    "I do not recognise the command you sent me. The available commands are:\n" + \
+    "    /start\n    /help\n    /register\n    /username\n    /leave"
+
+SENT_STICKER_MESSAGE = " sent a sticker."
+
+NOT_ADMIN_MESSAGE = PREFIX + "Hey! You are not an admin! Stop using admin commands! ):<"
+
+ADMIN_REMOVE_SUCCESS_MESSAGE = PREFIX + "User has successfully been removed."
+
+ADMIN_REMOVE_FAILURE_MESSAGE = PREFIX + \
+    "There was an error trying to remove the user. Please check with " + ADMIN_HANDLE + " immediately."
+
+INVALID_ADMIN_COMMAND_MESSAGE = PREFIX + "The admin command you gave me does not exist."
+
+UNDER_MAINTENANCE_MESSAGE = PREFIX + \
+    "I am currently under maintenance. >< Do hang tight and I will be back online soon!"
+
+
+
+# Other constants
 ANIMAL_LIST = ["iguana", "jackal", "jaguar", "kangaroo", "koala", "komodo", "kookaburra", "lemur", "leopard",
                "lion", "lizard", "llama", "manatee", "meerkat", "mongoose", "monkey", "moose", "nighthawk",
                "ocelot", "orca", "ostrich", "otter", "owl", "ox", "parrot", "peacock", "pelican", "penguin",
